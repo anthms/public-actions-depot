28809076af651f263f4178eff45d9a4c6ece7115
Anthony <86444119+anthms@users.noreply.github.com>
2022-12-16 16:36:18 -0600

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,7 +6,7 @@ function teardown {
   exit "${status}"
 }
 
-trap 'echo signal received!; kill $(jobs -p); wait; teardown' SIGINT SIGTERM
+trap 'trap " " SIGTERM; kill 0; wait; teardown' SIGINT SIGTERM
 
 echo "The script pid is $BASHPID"
 sleep 30 &
